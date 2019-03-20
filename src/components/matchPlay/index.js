1aa964ebdc4bd632b1036147051b7bdc1f498803
Franco Segura <franco@francos-mbp.attlocal.net>
2019-03-19 23:25:20 -0400

    changed timeline layout styling

--- src/components/matchPlay/index.js
+++ src/components/matchPlay/index.js
@@ -1,22 +1,47 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function MatchPlay({ playInfo }) {
-    const {minute,  player_name} = playInfo
-    // const {id, minute, play_type_code, player_name, player_number} = playInfo
+const MatchPlay = ({ playInfo }) => {
+    const {minute, play_type_name, player_name, player_number} = playInfo
+    console.log(playInfo)
     return (
         <li className="play-item">
             <div className="play-details">
                 <div className="play">
                     <div className="symbol">
-                        <span>{minute}'</span>
                     </div>
-                    <div className="description">
-                        {/* <span className="player-number">{ player_number }</span> */}
-                        <span className="player">{ player_name }</span>
-                    </div>                
+                    <div className="desc-wrapper">
+                        <div className="description">
+                            <div className="play-info">
+                                <div className="play-minute"><span>{minute}'</span></div>
+                                <div className="play-type">
+                                    <span>{play_type_name}</span>
+                                </div> 
+                            </div>
+                            <div className="player-info">
+                                <div className="player-name">
+                                    <span>{player_name}</span>
+                                </div>
+                                <div className="player-number">
+                                    <span>#{player_number}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <span className="v-line"></span>
         </li>
     )
 }
+
+MatchPlay.propTypes = {
+    playInfo: PropTypes.shape({
+        minute: PropTypes.number.isRequired,
+        play_type_name: PropTypes.string.isRequired,
+        player_name: PropTypes.string.isRequired,
+        player_number: PropTypes.number.isRequired
+    })
+}
+
+export default MatchPlay;
